151969709ca288f28172b425f20de672ab20cb60
Raveryk <raveryk@gmail.com>
2021-05-14 16:28:28 -0500

    Added basic input form items. Needs more styling and connecting values to POST.

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -1,9 +1,32 @@
+import TextField from '@material-ui/core/TextField';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 
 
 function AddMovie() {
 
     return(
         <>
+        <form>
+            <TextField label="Movie Title" variant="outlined"/>
+            <TextField label="Poster URL" variant="outlined"/>
+            <TextField label="Description" multiline rows={5} variant="outlined"/>
+            <Select value="Genre">
+                <MenuItem>Adventure</MenuItem>
+                <MenuItem>Animated</MenuItem>
+                <MenuItem>Biographical</MenuItem>
+                <MenuItem>Comedy</MenuItem>
+                <MenuItem>Disaster</MenuItem>
+                <MenuItem>Drama</MenuItem>
+                <MenuItem>Epic</MenuItem>
+                <MenuItem>Fantasy</MenuItem>
+                <MenuItem>Musical</MenuItem>
+                <MenuItem>Romantic</MenuItem>
+                <MenuItem>Sci-Fi</MenuItem>
+                <MenuItem>Space-Opera</MenuItem>
+                <MenuItem>Superhero</MenuItem>
+            </Select>
+        </form>
 
         </>
     )
