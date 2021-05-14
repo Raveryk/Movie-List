1a63a9d4786591129514e0cc8f0eeea56c23e121
Raveryk <raveryk@gmail.com>
2021-05-14 16:06:51 -0500

    Added functionality to Add Movie button to move to Add page.

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import './MovieList.css'
 
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
+import Button from '@material-ui/core/Button'
 
 function MovieList() {
 
     const dispatch = useDispatch();
+    const history = useHistory();
     const movies = useSelector(store => store.movies);
 
     useEffect(() => {
@@ -22,6 +25,8 @@ function MovieList() {
     return (
         <main>
             <h1>MovieList</h1>
+            <Button onClick={() => history.push('/add')}>Add Movie</Button>
+            
             
                 <section className="movies">
                     {movies.map(movie => {
